cdeaccf80dc2f30f446eb125ed19f7c656179581
Sadikul Islam <sadikulislam086@gmail.com>
2024-04-19 23:24:09 +0600

    added scraping

--- scraping.py
+++ scraping.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 
+
 url = "https://www.newegg.com/pny-geforce-rtx-4090-vcg409024tfxpb1/p/N82E16814133849"
 
 
@@ -10,4 +11,5 @@ doc = BeautifulSoup(response.text, "html.parser")
 prices = doc.find_all(text="$")
 parent = prices[0].parent
 strong = parent.find("strong")
+
 print(strong.string)
