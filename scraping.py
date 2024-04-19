4560149d2d50d6b6d563dd906b0785c5cce63d1e
Sadikul Islam <sadikulislam086@gmail.com>
2024-04-19 23:21:08 +0600

    first commit

---
+++ scraping.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://www.newegg.com/pny-geforce-rtx-4090-vcg409024tfxpb1/p/N82E16814133849"
+
+
+response = requests.get(url)
+doc = BeautifulSoup(response.text, "html.parser")
+
+prices = doc.find_all(text="$")
+parent = prices[0].parent
+strong = parent.find("strong")
+print(strong.string)
