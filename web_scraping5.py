3ab0070b8ab7157b01beed8098e7b68fae243eee
Sadikul Islam <sadikulislam086@gmail.com>
2024-04-21 09:19:54 +0600

    updated scraper

--- web_scraping5.py
+++ web_scraping5.py
@@ -1,19 +1,28 @@
-# Web Scraping with BeautifulSoup, Request, selenium
-import re
-import requests
+import pandas as pd
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
-url = 'https://oxylabs.io/blog'
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
+driver = webdriver.Chrome()
+driver.get('https://sandbox.oxylabs.io/products')
 
+results = []
+other_results = []
 
-# blog_titles = soup.find_all('a', class_='e1dscegp1')
-# dynamic rendering
+content = driver.page_source
+soup = BeautifulSoup(content, 'html.parser')
 
-# blog_titles = soup.find_all('a', class_=re.compile('oxy-rmqaiq'))
-# Using regex
+for a in soup.find_all(attrs={'class': 'product-card'}):
+    name = a.find('h4')
+    if name not in results:
+        results.append(name.text)
 
-# blog_titles = soup.select('a.e1dscegp1')
-# using CSS selectors
+for b in soup.find_all(attrs={'class': 'product-card'}):
+    name2 = b.find(attrs={'class': 'price-wrapper'})
+    if name2 not in other_results:
+        other_results.append(name2.text)
 
+series1 = pd.Series(results, name='Names')
+series2 = pd.Series(other_results, name='Prices')
+
+df = pd.DataFrame({'Names': series1, 'Prices': series2})
+df.to_csv('products.csv', index=False, encoding='utf-8')
